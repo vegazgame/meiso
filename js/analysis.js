2864a75f3c851ae4ad0bdf69c0910b3e59bbdc14
“Okgogogo” <btme@qq.com>
2020-01-27 23:13:43 +0800

    update 调整统计代码

--- js/analysis.js
+++
@@ -1,9 +0,0 @@
-<script>
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?e5e984b395b06e1d86483233d293baf8";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-</script>
